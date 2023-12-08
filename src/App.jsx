3f2ec5d5459c42b67e3575b5a40ccc25d5a03c64
NellyNel520 <channelharris520@gmail.com>
2023-12-08 14:29:55 -0500

    created routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-
+import { Route, Routes } from 'react-router-dom'
+// Components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+// Screens
+import Home from './screens/Home'
+import About from './screens/About'
+import AddReview from './screens/AddReview'
+import Catering from './screens/Catering'
+import Contact from './screens/Contact'
+import Gallery from './screens/Gallery'
+import Menu from './screens/Menu'
+import Reviews from './screens/Reviews'
+import Schedule from './screens/Schedule'
 import './index.css'
 
 function App() {
 
 
   return (
-    <>
-     hello
-    </>
+    <div className="App">
+    <Navbar />
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/addReview" element={<AddReview />} />
+        <Route path="/catering" element={<Catering />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="schedule" element={<Schedule />} />
+      </Routes>
+    </main>
+    <Footer />
+  </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				acme: ['Acme', 'sans-serif'],
+				lilita: ['Lilita One', 'sans-serif'],
+				monster: ['Montserrat Alternates', ' sans-serif'],
+				play: ['Playfair Display', 'serif'],
+				prata: ['Prata', 'serif'],
+				arch: ['Architects Daughter', 'cursive'],
+				beau: ['Beau Rivage', 'cursive'],
+				russo: ['Russo One', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 }
